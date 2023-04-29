3e6d2acbe9a732261c5abc0f297792a4367c31b7
handi.koswara <handi.koswara@technosoftautomotive.com>
2023-04-29 16:08:29 +0700

    add drag n drop

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -7,6 +7,8 @@ import CloseIcon from "@/icon/close";
 import CardForm from "./CardForm";
 import { useAtom } from "jotai";
 import { CardAtom } from "@/store/cardAtom";
+import { useDrop } from "react-dnd";
+import { changeStatusCard } from "@/lib/cardHelper";
 
 type BoxProps = {
     type: number
@@ -14,12 +16,27 @@ type BoxProps = {
 
 const Box = ({ type }: BoxProps) => {
     const [showAdd, setShowAdd] = useState(false);
-    const [cards,] = useAtom(CardAtom);
+    const [cards, setCards] = useAtom(CardAtom);
 
     const filterCards = cards.filter(e => e.status.id == type);
 
+    const onDropCard = (item: any) => {
+        const cardsTemp = JSON.parse(localStorage.getItem("cardData")!);
+        const result = changeStatusCard(item.id, type, cardsTemp);
+        setCards(result);
+    }
+
+    const [{ isOver }, drop] = useDrop(() => ({
+        accept: "card",
+        canDrop: (item: any) => { return item.status.id == type - 1 },
+        drop: (item: any) => { onDropCard(item) },
+        collect: (monitor) => ({
+            isOver: !!monitor.isOver()
+        })
+    }))
+
     return (
-        <div className="border border-gray-400 border-solid rounded-lg relative">
+        <div className="border border-gray-400 border-solid rounded-lg relative" ref={drop}>
             <div className="pl-1 pb-2 border-b solid border-gray-400 bg-gray">
                 {STATUS.find(e => e.id == type)?.label}
                 {type == 1 && !showAdd && <span className="inline-block cursor-pointer absolute top-2 right-1" onClick={() => setShowAdd(true)}><PlusIcon className="h-4 w-4 text-green-600" /></span>}
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,21 +2,29 @@ import CloseIcon from "@/icon/close";
 import { STATUS } from "@/lib/constants";
 import { CardProps } from "@/lib/types";
 import { CardAtom } from "@/store/cardAtom";
-import React, { useState } from "react";
+import React from "react";
 import { useAtom } from "jotai";
 import { changeStatusCard, deleteCard } from "@/lib/cardHelper";
+import { useDrag } from "react-dnd";
 
 const Card = ({ card }: CardProps) => {
     const { description, id, status, title } = card;
-    const [currentStatus, setCurrentStatus] = useState(status);
     const [cards, setCards] = useAtom(CardAtom);
+
+    const [{ isDragging }, drag] = useDrag(() => ({
+        type: "card",
+        item: { id, status },
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging(),
+        }),
+    }))
 
     const deleteCardById = () => {
         setCards(deleteCard(card.id, cards));
     }
 
     return (
-        <div className="p-2 border border-solid border-gray-400 rounded-lg bg-white mb-4">
+        <div className={`p-2 border border-solid ${isDragging ? "border-pink-400" : "border-gray-400"} rounded-lg bg-white mb-4`} ref={drag}>
             <div className="flex justify-between">
                 <div className="font-semibold break-words">{title}</div>
                 <div className="cursor-pointer" onClick={() => deleteCardById()}>
@@ -31,9 +39,9 @@ const Card = ({ card }: CardProps) => {
                     Status
                 </label>
                 <div>
-                    <select name={`status-${id}`} id={`status-${id}`} className="bg-white" value={card.status.id} onChange={e => setCards(changeStatusCard(card.id, Number(e.target.value), cards))}>
+                    <select name={`status-${id}`} id={`status-${id}`} className="bg-white" value={status.id} onChange={e => setCards(changeStatusCard(card.id, Number(e.target.value), cards))} disabled>
                         {STATUS.map(e => (
-                            <option value={e.id}>{e.label}</option>
+                            <option value={e.id} key={e.id + "-" + "id"}>{e.label}</option>
                         ))}
                     </select>
                 </div>
